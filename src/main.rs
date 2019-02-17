7ca5b78467df96bcdaac632ba0d4e596b8f48039
rschwalk <rschwalk@outlook.com>
2019-02-17 18:42:18 +0100

    Call the dht11 C lib.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use cty;
+
+extern "C" {
+    fn pi_2_dht_read(
+        typ: cty::c_int,
+        pin: cty::c_int,
+        humidity: *mut cty::c_float,
+        temperature: *mut cty::c_float
+        ) -> cty::c_int;
+}
 
 fn main() {
-    let res = 1;
+    let mut temp = 0.0;
+    let mut hum = 0.0;
 
-    println!("Result: {:?}", res);
+    let _val = unsafe {
+        pi_2_dht_read(11, 4, &mut hum, &mut temp);
+    };
+
+    println!("Temperature: {}C°, Humidity: {}%", temp, hum);
+
 }
 
